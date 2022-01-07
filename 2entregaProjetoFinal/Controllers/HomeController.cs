e72646b248196c08544cef358f42efd6e421507d
AlAnNuB <alannub6@gmail.com>
2022-01-06 23:40:16 -0300

    CadastroQuaseENavbar
    Cadastro faltando senha e login redes sociais e navbar faltando adicionar outras páginas

--- 2entregaProjetoFinal/Controllers/HomeController.cs
+++ 2entregaProjetoFinal/Controllers/HomeController.cs
@@ -9,29 +9,38 @@ using System.Threading.Tasks;
 
 namespace _2entregaProjetoFinal.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _logger = logger;
     }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Cadastro()
+    {
+      return View();
+    }
+    public IActionResult Contato()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
