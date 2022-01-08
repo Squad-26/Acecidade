44b2ccf7b0538a6e0feab6ff2ac3827fc84fc10f
JonasJesus <jonasdasilvajesus@outlook.com>
2022-01-07 21:22:30 -0300

    iniciando o menu lateral, com a animaçao ja funcionando

--- 2entregaProjetoFinal/wwwroot/js/site.js
+++ 2entregaProjetoFinal/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// script do menu lateral
+const botao = document.getElementById('btn-test')
+const menu = document.getElementById('1')
+
+botao.addEventListener("click", () =>{
+    console.log('evento')
+    if( menu.classList.contains("esconder")){
+        menu.classList.remove('esconder')
+        menu.classList.add('mostrar')
+    }else{
+        menu.classList.remove('mostrar')
+        menu.classList.add('esconder')
+    }
+})
