49aaf74e31d4233bb43669a3d78027b0c7adf11f
Squad-26 <97134993+Squad-26@users.noreply.github.com>
2022-01-08 01:02:59 -0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using _2entregaProjetoFinal.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2entregaProjetoFinal.Controllers
+{
+  public class HomeController : Microsoft.AspNetCore.Mvc.Controller
+    {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+      _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Cadastro()
+    {
+      return View();
+    }
+    public IActionResult Contato()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
